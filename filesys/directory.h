dc16ef705b7c8a592becb51e2fd4914d7afd3a18
ZWN2001 <2134356214@qq.com>
2022-11-04 20:21:02 +0800

    一些文件系统的注释

--- filesys/directory.h
+++ filesys/directory.h
@@ -1,25 +1,24 @@
 // directory.h 
 //	Data structures to manage a UNIX-like directory of file names.
-// 
+//
 //      A directory is a table of pairs: <file name, sector #>,
-//	giving the name of each file in the directory, and 
+//	giving the name of each file in the directory, and
 //	where to find its file header (the data structure describing
 //	where to find the file's data blocks) on disk.
-  //      We assume mutual exclusion is provided by the caller.
-//
-// Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
-// of liability and disclaimer of warranty provisions.
+//        We assume mutual exclusion is provided by the caller.
+//用于管理类似于 UNIX 文件目录（文件名的形式）的数据结构。
+//目录是一个表，表项为键值对: < file name, section # > ，
+// 给出目录中所有文件名，以及对应的在磁盘上它的文件头的位置(描述在哪里找到文件的数据块的数据结构)。
+//我们假设互斥锁是调用者提供的。
 
 #include "copyright.h"
 
-#ifndef DIRECTORY_H
+#ifndef DIRECTORY_H // if not define
 #define DIRECTORY_H
 
 #include "openfile.h"
 
-#define FileNameMaxLen 		9	// for simplicity, we assume 
-					// file names are <= 9 characters long
+#define FileNameMaxLen 9	//断言文件名长度不超过9位
 
 // The following class defines a "directory entry", representing a file
 // in the directory.  Each entry gives the name of the file, and where
@@ -28,13 +27,14 @@
 // Internal data structures kept public so that Directory operations can
 // access them directly.
 
+///下面的类定义了一个“目录条目”，表示目录中的一个文件。每个条目提供文件的名称，以及文件头在磁盘上的位置。
+///内部数据结构保持公开，以便 Directory 操作可以直接访问它们。
+
 class DirectoryEntry {
   public:
-    bool inUse;				// Is this directory entry in use?
-    int sector;				// Location on disk to find the 
-					//   FileHeader for this file 
-    char name[FileNameMaxLen + 1];	// Text name for file, with +1 for 
-					// the trailing '\0'
+    bool inUse;				//是否被占用
+    int sector;				// 文件的文件头在磁盘的位置
+    char name[FileNameMaxLen + 1];	// Text name for file, with +1 for the trailing '\0'
 };
 
 // The following class defines a UNIX-like "directory".  Each entry in
@@ -45,7 +45,11 @@ class DirectoryEntry {
 //
 // The constructor initializes a directory structure in memory; the
 // FetchFrom/WriteBack operations shuffle the directory information
-// from/to disk. 
+// from/to disk.
+
+///类 Directory 定义了一个Unix风格的文件目录，文件目录中的每个条目描述了一个文件及其起始位置
+///
+///文件目录的数据结构被存储在内存或者硬盘中。存储在硬盘中的时候是作为一个常规的Nachos文件。
 
 class Directory {
   public:
@@ -53,15 +57,18 @@ class Directory {
 					// with space for "size" files
     ~Directory();			// De-allocate the directory
 
+    ///从硬盘中初始化一个文件目录
     void FetchFrom(OpenFile *file);  	// Init directory contents from disk
+    ///将对目录内容的修改写入磁盘
     void WriteBack(OpenFile *file);	// Write modifications to 
 					// directory contents back to disk
-
+    ///查找名为name的文件的文件标头的扇区号
     int Find(char *name);		// Find the sector number of the 
 					// FileHeader for file: "name"
-
+    ///将名为name的文件加入文件目录
     bool Add(char *name, int newSector);  // Add a file name into the directory
 
+    ///将名为name的文件从目录中删除
     bool Remove(char *name);		// Remove a file from the directory
 
     void List();			// Print the names of all the files
